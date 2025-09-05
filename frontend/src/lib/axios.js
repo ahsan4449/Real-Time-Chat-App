8222382c0b40b593b9a34dfe5f35fa46713f52ea
Ahsan Mohd <ahsanmohd4449@gmail.com>
2025-09-05 15:21:31 +0530

    cors is changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-  baseURL: import.meta.env.MODE ="development"? "https://real-time-chat-app-2-9o6t.onrender.com/api":"/api",
+  baseURL:  "https://real-time-chat-app-2-9o6t.onrender.com/api",
   withCredentials:true,
 })
