d3cc77a22f8204ef5ca2030e876e9f7264fc5698
Ahsan Mohd <ahsanmohd4449@gmail.com>
2025-09-05 15:56:03 +0530

    //

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 //  BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8001" : "/";
 
 
-const BASE_URL="https://real-time-chat-app-2-9o6t.onrender.com";
+const BASE_URL="https://real-time-chat-app-2-9o6t.onrender.com/api";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
