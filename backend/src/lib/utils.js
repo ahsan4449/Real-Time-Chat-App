96cd3153bf53b3cfa1a9d0178d3dd969d87f91ed
Ahsan Mohd <ahsanmohd4449@gmail.com>
2025-09-05 15:13:50 +0530

    chANGED THE url

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,15 +1,34 @@
+// import jwt from "jsonwebtoken";
+
+// export const generateToken=(userId,res)=>{
+//    const token=jwt.sign({userId},process.env.JWT_SECRET,{
+//     expiresIn:"7d"
+//    })
+
+//    res.cookie("jwt",token,{
+//     maxAge:7*24*60*60*1000,
+//     httpOnly:true,
+//     sameSite:"strict",
+//     secure:process.env.NODE_ENV!=="development"
+//    })
+//    return token;
+// }
+
+
+
 import jwt from "jsonwebtoken";
 
-export const generateToken=(userId,res)=>{
-   const token=jwt.sign({userId},process.env.JWT_SECRET,{
-    expiresIn:"7d"
-   })
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-   res.cookie("jwt",token,{
-    maxAge:7*24*60*60*1000,
-    httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV!=="development"
-   })
-   return token;
-}
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // only true on Render
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // allow cross-site cookies
+  });
+
+  return token;
+};
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-  baseURL: import.meta.env.MODE ="development"? "http://localhost:8001/api":"/api",
+  baseURL: import.meta.env.MODE ="development"? "https://real-time-chat-app-2-9o6t.onrender.com/api":"/api",
   withCredentials:true,
 })
